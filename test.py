bb5adca0e22b12e8fdc2abe46d1cbbf63ed0e514
sauheiwong PC <sauheiwong@gmail.com>
2022-11-06 22:28:25 +0800

    add prime_check function in test.py

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-for i in range(10**1):
-    print(i)
+def prime_check(x):
+    for i in range(2,int(x**0.5)+1):
+        if x % i == 0:
+            return False
+    return True
+
+def last_number_check(x):
+    last_no = str(x)[-1]
+    if x == 2 or x == 5:
+        return True
+    elif int(last_no) in (2,4,5,6,8,0):
+        return False
+    else:
+        return prime_check(x)
+
+for i in range(2,10**1):
+    print(i, last_number_check(i))
